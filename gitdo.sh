85ebc2647232c6bbee0f4d7e785c7b148c89edbb
lamdevhs <lam.dev.hs@gmail.com>
2018-01-07 09:37:24 +0100

    gitdo.sh

--- gitdo.sh
+++ gitdo.sh
@@ -13,11 +13,15 @@ function br() {
 git config --global user.email lam.dev.hs@gmail.com
 git config --global user.name lamdevhs
 
+br; br; git status; git add $1
+br; git status
+br; echo -n "next step: commit -m $2 ; status " ; read
+
+br; git commit -m $2
+br; git status
+br; echo -n "next step: push " ; read
+
+br; git push origin master
+
+br; echo "end"
 br; br
-git status; git add $1; br; git status; br;
-echo -n "next step: commit -m $2 ; status " ; read
-
-br; git commit -m $2; br; git status; br;
-echo -n "next step: push " ; read
-
-br; git push origin master; br; echo "end"; br; br
