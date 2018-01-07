dd73b12c2fcecbb28e9549de5b86c67a1bd0238d
lamdevhs <lam.dev.hs@gmail.com>
2018-01-07 09:22:41 +0100

    gitdo.sh

--- gitcfg.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-git config --global user.email lam.dev.hs@gmail.com
-git config --global user.name lamdevhs
--- gitdo.sh
+++ gitdo.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-git add $1; git status; sleep 5
+# usage:
+# ./gitdo.sh <what to add> <commit name>
 
-git commit -m $2; git status; sleep 5
+# trick:
+# echo foo{,} --> foo foo
+
+git config --global user.email lam.dev.hs@gmail.com
+git config --global user.name lamdevhs
+
+git add $1; git status
+echo "next step: commit, status" ; sleep 5
+
+git commit -m $2; git status
+echo "next step: push" ; sleep 5
 
 git push origin master
