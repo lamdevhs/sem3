b4a6a5f58f1186a232ef003900f1be619bd61619
lamdevhs <lam.dev.hs@gmail.com>
2018-01-07 09:25:25 +0100

    gitdo.sh

--- gitdo.sh
+++ gitdo.sh
@@ -10,7 +10,7 @@ git config --global user.email lam.dev.hs@gmail.com
 git config --global user.name lamdevhs
 
 git add $1; git status
-echo "next step: commit, status" ; sleep 5
+echo "next step: commit -m $2 ; status" ; sleep 5
 
 git commit -m $2; git status
 echo "next step: push" ; sleep 5
